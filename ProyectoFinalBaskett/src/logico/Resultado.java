a58d5957b9b423fb544abf68f2ab48f5626e6471
bastian <bastian@bastian>
2025-03-27 13:50:00 -0400

    Eliminacion de clase y modificacion de metodos

--- ProyectoFinalBaskett/src/logico/Resultado.java
+++ ProyectoFinalBaskett/src/logico/Resultado.java
@@ -5,12 +5,10 @@ import java.util.ArrayList;
 public class Resultado {
     private int puntosLocal;
     private int puntosVisitante;
-    private ArrayList<EstadisticaJuego> estadisticasJugadores;
 
     public Resultado(int puntosLocal, int puntosVisitante) {
         this.puntosLocal = puntosLocal;
         this.puntosVisitante = puntosVisitante;
-        this.estadisticasJugadores = new ArrayList<>();
     }
 
     public int getPuntosLocal() {
@@ -21,23 +19,11 @@ public class Resultado {
         return puntosVisitante;
     }
 
-    public ArrayList<EstadisticaJuego> getEstadisticasJugadores() {
-        return estadisticasJugadores;
-    }
-
     public void setPuntosLocal(int puntosLocal) {
         this.puntosLocal = puntosLocal;
     }
 
     public void setPuntosVisitante(int puntosVisitante) {
         this.puntosVisitante = puntosVisitante;
-    }
-
-    public void setEstadisticasJugadores(ArrayList<EstadisticaJuego> estadisticasJugadores) {
-        this.estadisticasJugadores = estadisticasJugadores;
-    }
-    
-    public void agregarEstadisticaJugador(EstadisticaJuego estadistica) {
-        estadisticasJugadores.add(estadistica);
     }
 }
