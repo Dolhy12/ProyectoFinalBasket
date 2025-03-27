a966431a8c8f89f7005e8b432c03a9c366655248
USER <USER@DESKTOP-SK48N32>
2025-03-27 17:01:18 -0400

    aqui arregle la clase jugadoe, le agregue algunas cosas

--- ProyectoFinalBaskett/src/logico/Jugador.java
+++ ProyectoFinalBaskett/src/logico/Jugador.java
@@ -1,21 +1,30 @@
 package logico;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Jugador {
     private String ID;
     private String nombre;
     private int edad;
     private String posicion;
+    private String nacionalidad; 
+    private Date fechaDeNacimiento; 
+    private float peso; 
+    private float altura; 
     private int numero;
     private ArrayList<Lesion> lesiones;
     private EstadisticasJugador estadisticas;
 
-    public Jugador(String ID, String nombre, int edad, String posicion, int numero) {
+    public Jugador(String ID, String nombre, int edad, String posicion, String nacionalidad, Date fechaDeNacimiento, float peso, float altura, int numero) {
         this.ID = ID;
         this.nombre = nombre;
         this.edad = edad;
         this.posicion = posicion;
+        this.peso = peso; 
+        this.nacionalidad = nacionalidad; 
+        this.fechaDeNacimiento = fechaDeNacimiento;
+        this.altura = altura; 
         this.numero = numero;
         this.lesiones = new ArrayList<>();
         this.estadisticas = new EstadisticasJugador();
@@ -36,7 +45,7 @@ public class Jugador {
     public String getPosicion() {
         return posicion;
     }
-
+    
     public int getNumero() {
         return numero;
     }
@@ -80,4 +89,36 @@ public class Jugador {
     public void agregarLesion(Lesion lesion) {
         lesiones.add(lesion);
     }
+
+	public float getAltura() {
+		return altura;
+	}
+
+	public void setAltura(float altura) {
+		this.altura = altura;
+	}
+
+	public float getPeso() {
+		return peso;
+	}
+
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+
+	public Date getFechaDeNacimiento() {
+		return fechaDeNacimiento;
+	}
+
+	public void setFechaDeNacimiento(Date fechaDeNacimiento) {
+		this.fechaDeNacimiento = fechaDeNacimiento;
+	}
 }
